9940e0e9ca2425f1f4b39ed66ae44145ed3152d5
hjboss <hongjiangproject@gmail.com>
2021-02-28 14:00:18 +0800

    Initial commit

--- github.ps1
+++ github.ps1
@@ -6,40 +6,86 @@ license that can be found in the LICNESE file.
 
 Git(Github.com) Client Commands.
 
-@authors hjboss <hongjiangproject@gmail.com> 2018-04 $$
+@authors hjboss <hongjiangproject@gmail.com> 2021-03 $$
 @version 1.0.0
 #>
-switch ($args[0]) {
+switch ($args[0])
+{
   'add'
   {
-    if (($args.Count -lt 2) -or ($args[1] -eq '*'))
+    if ($args.Count -eq 2)
     {
-      git add --all
-    }
-    else
-    {
-      for ($idx = 1; $idx -lt $args.Count; $idx++)
+      if ((Test-Path -Path $args[1]) -eq $True)
       {
-        git add $args[$idx]
+        git add $args[1]
       }
+
+      Exit
     }
+
+    Write-Host '用法: github add <filename>'
     break
   }
   'ci'
   {
-    if ($args.Count -lt 2)
+    if ($args.Count -lt 3)
     {
-      git commit -a -m 'Initial commit'
-    }
-    else
-    {
-      git commit -a -m $args[1]
+      if ($args.Count -lt 2)
+      {
+        git commit -a -m 'Initial commit'
+      }
+      else
+      {
+        git commit -a -m $args[1]
+      }
+
+      git push -u origin master
+      Exit
     }
 
-    git push -u origin master
+    Write-Host '用法: github ci [comment]'
     break
   }
-  Default
+  'co'
+  {
+    if ($args.Count -ge 3)
+    {
+      if ($args[3] -like '*:*')
+      {
+        git clone "https://$($args[3])@github.com/$($args[1])/$($args[2]).git"
+      }
+      else
+      {
+        git clone "https://github.com/$($args[1])/$($args[2]).git"
+      }
+
+      Exit
+    }
+
+    Write-Host '用法: github co <owner> <repository> [user:pass]'
+    break
+  }
+  'rm'
+  {
+    
+  }
+  'up'
+  {
+
+  }
+  'st'
+  {
+
+  }
+  'help'
+  {
+
+  }
+  'version'
+  {
+
+  }
+  default
   {
     Write-Host '使用github help得到用法'
   }
